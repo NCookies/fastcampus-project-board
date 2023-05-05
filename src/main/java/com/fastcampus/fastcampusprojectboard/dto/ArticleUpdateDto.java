0a79c9f752b553e237aeb46ce42be357fe081af1
SeungWoo Ryu <swstar21c@gmail.com>
2023-05-05 20:14:42 +0900

    #19 - 게시글 서비스 로직의 테스트와 골격 잡기
    다음의 기능을 테스트로 표현
    
    * 게시글 검색하면 게시글 리스트 반환
    * 게시글을 ID로 조회
    * 게시글 생성
    * 게시글 수정
    * 게시글 삭제
    
    필요한 dto 와 enum을 함께 작성했는데,
    추후에 수정할 수도 있음

---
+++ src/main/java/com/fastcampus/fastcampusprojectboard/dto/ArticleUpdateDto.java
@@ -0,0 +1,14 @@
+package com.fastcampus.fastcampusprojectboard.dto;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public record ArticleUpdateDto (
+    String title,
+    String content,
+    String hashTag
+) {
+    public static ArticleUpdateDto of(String title, String content, String hashTag) {
+        return new ArticleUpdateDto(title, content, hashTag);
+    }
+}
