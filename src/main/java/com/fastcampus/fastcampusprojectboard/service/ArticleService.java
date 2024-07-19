e11c4c99ead5fb76af259844e3f3f948e56f4b32
SeungWoo Ryu <swstar21c@gmail.com>
2024-07-20 04:03:02 +0900

    #19 - 비즈니스 로직 구현
    테스트를 토대로 게시판 서비스에 필요한 비즈니스 로직 구현 완료

--- src/main/java/com/fastcampus/fastcampusprojectboard/service/ArticleService.java
+++ src/main/java/com/fastcampus/fastcampusprojectboard/service/ArticleService.java
@@ -1,15 +1,20 @@
 package com.fastcampus.fastcampusprojectboard.service;
 
+import com.fastcampus.fastcampusprojectboard.domain.Article;
 import com.fastcampus.fastcampusprojectboard.domain.type.SearchType;
 import com.fastcampus.fastcampusprojectboard.dto.ArticleDto;
 import com.fastcampus.fastcampusprojectboard.dto.ArticleWithCommentsDto;
 import com.fastcampus.fastcampusprojectboard.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
+
+@Slf4j
 @RequiredArgsConstructor
 @Transactional
 @Service
@@ -18,22 +23,49 @@ public class ArticleService {
 
     @Transactional(readOnly = true)
     public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
-        return Page.empty();
+        if (searchKeyword == null || searchKeyword.isBlank()) {
+            // 서비스는 도메인 코드와 article-dto까지만 알고 있다.
+            // 실제로 컨트롤러가 어떤 데이터를 보여주는지는 모른다.
+            return articleRepository.findAll(pageable).map(ArticleDto::from);
+        }
+
+        return switch (searchType) {
+            case TITLE -> articleRepository.findByTitleContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case CONTENT -> articleRepository.findByContentContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case ID -> articleRepository.findByUserAccount_UserIdContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case NICKNAME -> articleRepository.findByUserAccount_NicknameContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case HASHTAG -> articleRepository.findByHashtag("#" + searchKeyword, pageable).map(ArticleDto::from);
+        };
     }
 
     @Transactional(readOnly = true)
     public ArticleWithCommentsDto getArticle(Long articleId) {
-        return null;
+        return articleRepository.findById(articleId)
+                .map(ArticleWithCommentsDto::from)
+                .orElseThrow(() -> new EntityNotFoundException("게시글이 없습니다 - articleId: " + articleId));
     }
 
     public void saveArticle(ArticleDto dto) {
-
+        articleRepository.save(dto.toEntity());
     }
 
+    // 클래스 레벨 @Transactional에 의해서 메소드 단위로 트랜잭션이 묶여 있다.
+    // 그래서 트랜잭션이 끝날 때 영속성 컨텍스트는 article이 변한 것을 감지해낸다.
+    // 그 감지 부분에 대해 쿼리를 날리기 때문에 save() 메소드를 호출하지 않아도 된다.
+    // 만약 코드 단위로 업데이트 한다는 것을 명시하고 싶다면 save() 또는 saveAndFlush()를 사용해도 된다.
     public void updateArticle(ArticleDto dto) {
-
+        try {
+            // findById와 다르게 select 쿼리를 사용하지 않고 조회할 수 있다.
+            Article article = articleRepository.getReferenceById(dto.id());
+            if (dto.title() != null) { article.setTitle(dto.title()); }
+            if (dto.content() != null) { article.setContent(dto.content()); }
+            article.setHashtag(dto.hashtag());
+        } catch (EntityNotFoundException e) {
+            log.warn("게시글 업데이트 실패. 게시글을 찾을 수 없습니다 - dto: {}", dto);
+        }
     }
 
     public void deleteArticle(long articleId) {
+        articleRepository.deleteById(articleId);
     }
 }
