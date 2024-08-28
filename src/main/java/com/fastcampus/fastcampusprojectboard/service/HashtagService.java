4f7088b7bce08aadc1245fd5141c60e6f55b7887
NCookies <swstar21c@gmail.com>
2024-08-28 22:30:27 +0900

    #54 - 도메인 변경점의 반영: 해시태그
    - 해시태그 엔티티가 추가되고, 게시글이 해시태그를 여러 개 가질 수 있도록 설계가 변경됐다.
      - 이 때문에 변경점이 크게 전파되었다.
      - 커밋을 컴파일 에러 상태로 등록하는 것좋지 않아 어쩔 수 없이 변경점을 모두 한 커밋에 담았다.
      - 이 방법이 괜찮을지는 추후 더 고민해보자.
    - 도메인 변경만 반영하고 실제 기능은 구현하지 않았으므로 컴파일 에러만 피할 뿐, 테스트는 여전히 실패하는 상태이다.

---
+++ src/main/java/com/fastcampus/fastcampusprojectboard/service/HashtagService.java
@@ -0,0 +1,17 @@
+package com.fastcampus.fastcampusprojectboard.service;
+
+import java.util.Set;
+
+public class HashtagService {
+    public Object parseHashtagNames(String content) {
+        return null;
+    }
+
+    public Object findHashtagsByNames(Set<String> expectedHashtagNames) {
+        return null;
+    }
+
+    public void deleteHashtagWithoutArticles(Object any) {
+
+    }
+}
--- src/test/java/com/fastcampus/fastcampusprojectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcampus/fastcampusprojectboard/controller/ArticleControllerTest.java
@@ -5,6 +5,7 @@ import com.fastcampus.fastcampusprojectboard.domain.constant.FormStatus;
 import com.fastcampus.fastcampusprojectboard.domain.constant.SearchType;
 import com.fastcampus.fastcampusprojectboard.dto.ArticleDto;
 import com.fastcampus.fastcampusprojectboard.dto.ArticleWithCommentsDto;
+import com.fastcampus.fastcampusprojectboard.dto.HashtagDto;
 import com.fastcampus.fastcampusprojectboard.dto.UserAccountDto;
 import com.fastcampus.fastcampusprojectboard.dto.request.ArticleRequest;
 import com.fastcampus.fastcampusprojectboard.response.ArticleResponse;
@@ -73,7 +74,9 @@ class ArticleControllerTest {
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"))
-                .andExpect(model().attributeExists("paginationBarNumbers"));
+                .andExpect(model().attributeExists("paginationBarNumbers"))
+                .andExpect(model().attributeExists("searchTypes"))
+                .andExpect(model().attribute("searchTypeHashtag", SearchType.HASHTAG));
         then(articleService).should().searchArticles(eq(null), eq(null), any(Pageable.class));
         then(paginationService).should().getPaginationBarNumbers(anyInt(), anyInt());
     }
@@ -237,7 +240,7 @@ class ArticleControllerTest {
     @WithMockUser
     @DisplayName("[view][GET] 새 게시글 작성 페이지")
     @Test
-    void givenAuthorizedUser_whenRequesting_thenReturnsNewArticlePage() throws Exception {
+    void givenNothing_whenRequesting_thenReturnsNewArticlePage() throws Exception {
         // Given
 
         // When & Then
@@ -255,7 +258,7 @@ class ArticleControllerTest {
     @Test
     void givenNewArticleInfo_whenRequesting_thenSavesNewArticle() throws Exception {
         // Given
-        ArticleRequest articleRequest = ArticleRequest.of("new title", "new content", "#new");
+        ArticleRequest articleRequest = ArticleRequest.of("new title", "new content");
         willDoNothing().given(articleService).saveArticle(any(ArticleDto.class));
 
         // When & Then
@@ -274,7 +277,7 @@ class ArticleControllerTest {
     @WithMockUser
     @DisplayName("[view][GET] 게시글 수정 페이지 - 정상 호출, 인증된 사용자")
     @Test
-    void givenNothing_whenRequesting_thenReturnsUpdatedArticlePage() throws Exception {
+    void givenAuthorizedUser_whenRequesting_thenReturnsUpdatedArticlePage() throws Exception {
         // Given
         long articleId = 1L;
         ArticleDto dto = createArticleDto();
@@ -296,7 +299,7 @@ class ArticleControllerTest {
     void givenUpdatedArticleInfo_whenRequesting_thenUpdatesNewArticle() throws Exception {
         // Given
         long articleId = 1L;
-        ArticleRequest articleRequest = ArticleRequest.of("new title", "new content", "#new");
+        ArticleRequest articleRequest = ArticleRequest.of("new title", "new content");
         willDoNothing().given(articleService).updateArticle(eq(articleId), any(ArticleDto.class));
 
         // When & Then
@@ -339,7 +342,7 @@ class ArticleControllerTest {
                 createUserAccountDto(),
                 "title",
                 "content",
-                "#java"
+                Set.of(HashtagDto.of("java"))
         );
     }
 
@@ -350,7 +353,7 @@ class ArticleControllerTest {
                 Set.of(),
                 "title",
                 "content",
-                "#java",
+                Set.of(HashtagDto.of("java")),
                 LocalDateTime.now(),
                 "uno",
                 LocalDateTime.now(),
