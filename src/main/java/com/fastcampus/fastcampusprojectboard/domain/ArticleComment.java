df650ded8b3855a049a910bb313154b207fd7228
NCookies <swstar21c@gmail.com>
2024-09-23 19:51:55 +0900

    #64 - 대댓글 도메인 설계
    - 대댓글 도메인 안에서 부모, 자식 관계를 설정하는 코드를 추가
    - 자식 댓글의 컬렉션 변화가 쿼리에 반영되게끔 cascading 규칙을 모두 적용
    - 이번엔 단방향 연관관계 설정을 사용해보기로 함
    - 따라서 부모 댓글은 엔티티가 아닌 `Long`으로 id를 직접 표현
      - 또한 자식 댓글을 추가할 수 있는 메소드 추가 제공

--- src/main/java/com/fastcampus/fastcampusprojectboard/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/fastcampusprojectboard/domain/ArticleComment.java
@@ -6,7 +6,9 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @ToString(callSuper = true)
@@ -31,19 +33,37 @@ public class ArticleComment extends AuditingFields {
     @ManyToOne(optional = false)
     private UserAccount userAccount; // 유저 정보 (ID)
 
+    @Setter
+    @Column(updatable = false)      // 부모 댓글이 바뀔 일은 없음
+    private Long parentCommentId;   // 부모 댓글 ID
+
+    @ToString.Exclude
+    @OrderBy("createdAt ASC")
+    @OneToMany(mappedBy = "parentCommentId",    // 부모 댓글 -> 자식 댓글 단방향으로만 참조할 수 있음
+            cascade = CascadeType.ALL)          // 부모 댓글이 삭제되면 자식 댓글도 함께 삭제
+    private Set<ArticleComment> childComments = new LinkedHashSet<>();
+
     @Setter @Column(nullable = false, length = 500) private String content; // 본문
 
 
     protected ArticleComment() {}
 
-    private ArticleComment(Article article, UserAccount userAccount, String content) {
+    private ArticleComment(Article article, UserAccount userAccount, Long parentCommentId, String content) {
         this.article = article;
         this.userAccount = userAccount;
+        this.parentCommentId = parentCommentId;
         this.content = content;
     }
 
     public static ArticleComment of(Article article, UserAccount userAccount, String content) {
-        return new ArticleComment(article, userAccount, content);
+        return new ArticleComment(article, userAccount, null, content);
+    }
+
+    public void addChildComment(ArticleComment child) {
+        // CASCADE 옵션 덕분에 부모 댓글에서 자식 댓글을 추가하기만 해도
+        // 자식 댓글과의 연관 관계가 자동으로 생성된다.
+        child.setParentCommentId(this.getId());
+        this.getChildComments().add(child);
     }
 
     @Override
