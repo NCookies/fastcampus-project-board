1b7f6df094d247b1d094c03be76185ce72840190
SeungWoo Ryu <swstar21c@gmail.com>
2024-10-02 23:35:36 +0900

    #66 - 대댓글 비즈니스 로직 구현
    - 다음과 같은 내용을 구현함
      - 부모/자식 관계를 만들면서 entity <-> dto 간 변환 로직
      - 댓글 서비스에서 댓글과 대댓글을 저장하고 삭제하는 방법
      - 댓글과 대댓글을 db 데이터로부터 변환할 때 서로 계층 구조를 만들고 각각 정렬하는 과정

--- src/main/java/com/fastcampus/fastcampusprojectboard/dto/response/ArticleCommentResponse.java
+++ src/main/java/com/fastcampus/fastcampusprojectboard/dto/response/ArticleCommentResponse.java
@@ -3,6 +3,9 @@ package com.fastcampus.fastcampusprojectboard.dto.response;
 import com.fastcampus.fastcampusprojectboard.dto.ArticleCommentDto;
 
 import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
 
 public record ArticleCommentResponse(
         Long id,
@@ -10,7 +13,9 @@ public record ArticleCommentResponse(
         LocalDateTime createdAt,
         String email,
         String nickname,
-        String userId
+        String userId,
+        Long parentCommentId,
+        Set<ArticleCommentResponse> childComments
 ) {
 
     public static ArticleCommentResponse of(Long id,
@@ -19,7 +24,32 @@ public record ArticleCommentResponse(
                                             String email,
                                             String nickname,
                                             String userId) {
-        return new ArticleCommentResponse(id, content, createdAt, email, nickname, userId);
+        return ArticleCommentResponse.of(id, content, createdAt, email, nickname, userId, null);
+    }
+
+    public static ArticleCommentResponse of(Long id,
+                                            String content,
+                                            LocalDateTime createdAt,
+                                            String email,
+                                            String nickname,
+                                            String userId,
+                                            Long parentCommentId) {
+
+        // 대댓글은 createdAt 기준으로 오름차순 정렬되도록 한다.
+        // 만약 createdAt이 동일하다면 (거의 그럴 일은 없겠지만) id 순으로 정렬한다.
+        Comparator<ArticleCommentResponse> childCommentComparator = Comparator
+                .comparing(ArticleCommentResponse::createdAt)
+                .thenComparingLong(ArticleCommentResponse::id);
+
+        // 정렬 순서를 보장하기 위해 TreeSet을 사용한다.
+        return new ArticleCommentResponse(id,
+                content,
+                createdAt,
+                email,
+                nickname,
+                userId,
+                parentCommentId,
+                new TreeSet<>(childCommentComparator));
     }
 
     public static ArticleCommentResponse from(ArticleCommentDto dto) {
@@ -28,14 +58,19 @@ public record ArticleCommentResponse(
             nickname = dto.userAccountDto().userId();
         }
 
-        return new ArticleCommentResponse(
+        return ArticleCommentResponse.of(
                 dto.id(),
                 dto.content(),
                 dto.createdAt(),
                 dto.userAccountDto().email(),
                 nickname,
-                dto.userAccountDto().userId()
+                dto.userAccountDto().userId(),
+                dto.parentCommentId()
         );
+    }
+
+    public boolean hasParentComment() {
+        return parentCommentId != null;
     }
 
 }
