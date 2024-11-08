b314805f99d8a6882974397eb1abccbecbcaa86a
SeungWoo Ryu <swstar21c@gmail.com>
2024-11-08 15:25:13 +0900

    #98 - UserAccount api 노출 추가 + data rest projection을 이용한 데이터 노출 필드 설정
    - `UserAccount`를 노출할 api를 추가로 제공한다.
    - 이렇게 하면 다른 게시글, 댓글 등의 data rest api 노출 데이터 형식이 원하지 않는 스타일로 바뀌게 된다.
      - 데이터가 직접 들어가있는 것이 아니라 링크로 제공된다. => 통신 i/o 낭비
      - 이를 수동으로 설정하기 위해 projection 기능을 사용하여 노출할 필드들을 직접 지정했다.

---
+++ src/main/java/com/fastcampus/fastcampusprojectboard/domain/projection/UserAccountProjection.java
@@ -0,0 +1,19 @@
+package com.fastcampus.fastcampusprojectboard.domain.projection;
+
+import com.fastcampus.fastcampusprojectboard.domain.UserAccount;
+import org.springframework.data.rest.core.config.Projection;
+
+import java.time.LocalDateTime;
+
+// API 문서에서 확인할 수 있는 정보들을 직접 설정
+@Projection(name = "withoutPassword", types = { UserAccount.class })
+public interface UserAccountProjection {
+    Long getUserId();
+    String getEmail();
+    String getNickname();
+    String getMemo();
+    LocalDateTime getCreatedAt();
+    String getCreatedBy();
+    LocalDateTime getModifiedAt();
+    String getModifiedBy();
+}
--- src/main/java/com/fastcampus/fastcampusprojectboard/repository/UserAccountRepository.java
+++ src/main/java/com/fastcampus/fastcampusprojectboard/repository/UserAccountRepository.java
@@ -1,7 +1,10 @@
 package com.fastcampus.fastcampusprojectboard.repository;
 
 import com.fastcampus.fastcampusprojectboard.domain.UserAccount;
+import com.fastcampus.fastcampusprojectboard.domain.projection.UserAccountProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(excerptProjection = UserAccountProjection.class)
 public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
 }
